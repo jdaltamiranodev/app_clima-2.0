9bd39ce93e0316f7d1c51e79cd04763eef9fd5b3
l6898 <espinolalourdes83@gmail.com>
2024-04-19 14:13:45 +0200

    single day

--- clima2.0/src/components/DailyWeather.vue
+++ clima2.0/src/components/DailyWeather.vue
@@ -2,10 +2,13 @@
     <div class="dailyCard">
     <h2>{{msg}}</h2>
         <div class="SingleDayCard" >
-            
+            <div class="rightNow">
+              <h2 v-if="weatherData && weatherData.name">{{ weatherData.name}}</h2>
 
-
-
+               <!--  <h2 v-if="weatherData && weatherData.name">{{ Math.floor(weatherData.name)}}</h2> -->
+                <p v-if="weatherData && weatherData.main">Temperatura: {{ weatherData.main.temp }}°C</p>
+                <p v-if="weatherData && weatherData.weather && weatherData.weather[0]">Descripción: {{ weatherData.weather[0].description }}</p>
+            </div>
             <div class="note">
                 <img alt="" src="../assets/pen.svg" >
                 <textarea name="" id="notes" cols="30" rows="10" placeholder="escribe aquí tu tarea"></textarea>
@@ -18,17 +21,40 @@
         </div>
     </div>
 
-
 </template>
 
-<script>
-export default {
-    name: 'DailyWeather',
-    props:{
-        msg:String
-    }
-} 
+<script> 
+import { defineComponent,ref, onMounted } from 'vue'; 
+import axios from 'axios';
+
+export default defineComponent({
+  name: 'DailyWeather',
+  setup() {
+    const weatherData = ref(null);
+    const apiKey = '8f5a97309913a1742cefe19e754e0110';
+
+
+
+   // Método para obtener los datos del clima
+   const getWeatherData = async () => {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`);
+        weatherData.value = response.data;
+      } catch (error) {
+        console.error('Error al obtener los datos del clima:', error);
+      }
+    };
+
+    // Llamar al método cuando el componente se monta
+    onMounted(() => {
+      getWeatherData();
+    });
+
+    return { weatherData };
+  },
+});
 </script>
+
 <style >
 .dailyCard{ border: solid 2px black; background-color: #d9d9d9 ;
 
--- clima2.0/src/components/NewsWords.vue
+++ clima2.0/src/components/NewsWords.vue
@@ -1,35 +1,39 @@
 <!-- News.vue -->
 <template>
   <div class="news">
-    <h2>{{ title }}</h2>
-    <p>{{ content }}</p>
+    <h2 v-if="weatherData && weatherData.name">Condiciones meteorológicas actuales en {{ weatherData.name }}</h2>
+    <p v-if="weatherData && weatherData.main">Temperatura: {{ weatherData.main.temp }}°C</p>
+    <p v-if="weatherData && weatherData.weather && weatherData.weather[0]">Descripción: {{ weatherData.weather[0].description }}</p>
   </div>
 </template>
 
-<script setup>
-// Importa reactive para crear datos reactivos
-/* import { reactive } from 'vue'; */
+<script>
+import { defineComponent, ref, onMounted } from 'vue';
+import axios from 'axios';
 
-// Define los datos reactivos del componente
-/*const state = reactive({
-  title: 'Título de la noticia',
-  content: 'Contenido de la noticia...'
-});*/
-
-/* export default {
+export default defineComponent({
   name: 'NewsWords',
-  props: { title: 'Título de la noticia',
-  content: 'Contenido de la noticia...'
-    
-  }
-} */
+  setup() {
+    const weatherData = ref(null);
+    const apiKey = '8f5a97309913a1742cefe19e754e0110';
 
-const title = "Título de la notici"
-const content = "Contenido de la noticia..."
+    // Método para obtener los datos del clima
+    const getWeatherData = async () => {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`);
+        weatherData.value = response.data;
+      } catch (error) {
+        console.error('Error al obtener los datos del clima:', error);
+      }
+    };
+
+    // Llamar al método cuando el componente se monta
+    onMounted(() => {
+      getWeatherData();
+    });
+
+    return { weatherData };
+  },
+});
 </script>
 
-<style scoped>
-
-.news { }
-  
-</style>
