d5f24df6eaaad5bc92b6b830cb751c216b4c0a71
l6898 <cdespinolalourdes83@gmail.com>
2024-04-21 13:28:35 +0200

    vista agenda

--- clima2.0/src/App.vue
+++ clima2.0/src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
     <header>
-      <h1>My Vue App</h1>
-      <nav>
+      
+      <nav class="container flex flex-col center ">
         
-        <router-link to="/">Home</router-link>
-        <router-link to="/menu">Menu</router-link>
-        <router-link to="/Noticias">Noticias</router-link>
+        <router-link to="/home">Home</router-link>     |
+        <router-link to="/menu">Menu</router-link>     |
+        <router-link to="/Noticias">Noticias</router-link>     |
+        <router-link to="/Agenda">Agenda</router-link>     |
       </nav>
     </header>
 
@@ -29,38 +30,4 @@ export default defineComponent({
   
 });
 </script>
-
-<style>
-/* Estilos globales de la aplicación */
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-/* Estilos para el encabezado y la navegación */
-header {
-  background-color: #f8f9fa;
-  padding: 10px;
-  border-bottom: 1px solid #ccc;
-}
-
-nav {
-  display: flex;
-  justify-content: center;
-}
-
-nav a {
-  margin: 0 10px;
-  text-decoration: none;
-  color: #333;
-}
-
-nav a:hover {
-  text-decoration: underline;
-}
-</style>
 
--- clima2.0/src/main.js
+++ clima2.0/src/main.js
@@ -4,13 +4,15 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from './views/HomeClima.vue';
 import Menu from './views/MenuClima.vue';
 import NoticiasWords from './views/NoticiasWords.vue';
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+ import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css' 
+import HelloWorld from './components/HelloWorld.vue';
 
 const routes = [
     { path: '/', component: Home },
     { path: '/menu', component: Menu },
     { path: '/Noticias', component: NoticiasWords },
+    { path: '/Agenda', component:HelloWorld, },
   ];
 
   const router = createRouter({
